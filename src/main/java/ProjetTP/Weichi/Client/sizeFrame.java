34572fe536c3f4250f6d952067cea3917f6e3409
buczekxpompa <bulpopulbo@interia.pl>
2019-12-03 16:17:16 +0100

    Buttony

---
+++ src/main/java/ProjetTP/Weichi/Client/sizeFrame.java
@@ -0,0 +1,28 @@
+package ProjetTP.Weichi.Client;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class sizeFrame extends JFrame {
+
+        public static final int WYS = 125;
+        public static final int SZER = 300;
+        private JButton nine;
+        private JButton thirteen;
+        private JButton nineteen;
+
+        public sizeFrame()
+        {
+            nine = new JButton("9x9");
+            thirteen = new JButton("13x13");
+            nineteen = new JButton("19x19");
+
+            setLayout(new FlowLayout());
+            setPreferredSize(new Dimension(SZER, WYS));
+            add(nine);
+            add(thirteen);
+            add(nineteen);
+
+            setVisible(true);
+        }
+}
