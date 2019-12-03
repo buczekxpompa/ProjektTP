c2a102fbd9e1b552f2ac0b9966c473eb5e2abbdf
Fermento99 <pk.pawlak99@gmail.com>
2019-12-03 15:39:37 +0100

    2nd initial commit

---
+++ src/main/java/ProjetTP/Weichi/Server/Server.java
@@ -0,0 +1,15 @@
+package ProjetTP.Weichi.Server;
+
+public class Server extends Thread {
+
+    @Override
+    public void run() {
+        //TODO: make connection
+
+        //TODO: make the game
+
+        //TODO: do logic
+
+        // rematch?
+    }
+}
--- src/test/java/ProjetTP/Weichi/AppTest.java
+++ src/test/java/ProjetTP/Weichi/AppTest.java
@@ -1,4 +1,4 @@
-package ProjetTP.Wei-chi;
+package ProjetTP.Weichi;
 
 import static org.junit.Assert.assertTrue;
 
