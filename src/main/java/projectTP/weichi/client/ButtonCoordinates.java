7d2d32cc0d5058ed051c1c7bf470ba77aa653cf2
buczekxpompa <bulpopulbo@interia.pl>
2019-12-04 16:19:35 +0100

    po 5 koła se podjade moj czlowiek donald ma juz taką dziabe wszystko już widziałem co potrzebne żeby grać w gre jak na sylwestrze

---
+++ src/main/java/projectTP/weichi/client/ButtonCoordinates.java
@@ -0,0 +1,16 @@
+package projectTP.weichi.client;
+
+import javax.swing.*;
+
+public class ButtonCoordinates extends JButton
+{
+    private int x;
+    private int y;
+
+    public ButtonCoordinates(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+}
--- src/main/java/projectTP/weichi/client/frames/GameFrame.java
+++ src/main/java/projectTP/weichi/client/frames/GameFrame.java
@@ -1,4 +1,20 @@
 package projectTP.weichi.client.frames;
 
+import projectTP.weichi.client.ButtonCoordinates;
+
+import javax.swing.*;
+import java.awt.*;
+
 public class GameFrame {
+    GameFrame(int size)
+    {
+        JPanel field = new JPanel();
+        for(int i=0; i<size; i++)
+        {
+            for(int j=0; j<size; j++)
+            {
+                field.add(new ButtonCoordinates(i, j));
+            }
+        }
+    }
 }
